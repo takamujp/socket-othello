1320e5ea201a57f74c8c534e99cf0c4872f9ed0f
takamujp <afbmk248@oct.zaq.ne.jp>
2013-09-13 17:01:04 +0900

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
 (function () {
-    window.Othello.Socket = io.connect("http://mumu.sharuru07.jp:3100/",  {"sync disconnect on unload" : true});
+    window.Othello.Socket = io.connect(window.location.href,  {"sync disconnect on unload" : true});
     
     var router = new window.Othello.Router(),
         app = new window.Othello.View.App({router: router});
